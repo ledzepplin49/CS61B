1b034afa31ab0ada84d3b4308ecb912cf0df38f0
oasis <oasisliveforever02@gmail.com>
2026-02-02 01:26:50 +0800

    done with lab2,fix null edge case

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,13 +10,14 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
+        if(head ==null){
+            return ;
+        }
         while (head.rest != null ) {
             head.first += c;
             head = head.rest;
         }
-        if(head !=null){
-            head.first += c;
-        }
+        head.first += c;
     }
 
     /**
