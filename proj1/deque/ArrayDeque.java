fe2c4b65cef2d2ef5e323f911dfc08eb9a9ed083
oasis <oasisliveforever02@gmail.com>
2026-04-12 10:39:13 +0800

    fix some error

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,29 +2,34 @@ package deque;
 
 import java.util.Iterator;
 
-public class ArrayDeque <T>implements Iterable<T>,Deque<T> {
+public class ArrayDeque<T> implements Iterable<T>, Deque<T> {
     private T[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
-    public ArrayDeque(){
-        this.items=(T[]) new Object[10];;
-        this.size=0;
-        this.nextFirst=4;
-        this.nextLast=5;
+
+    public ArrayDeque() {
+        this.items = (T[]) new Object[10];
+        ;
+        this.size = 0;
+        this.nextFirst = 4;
+        this.nextLast = 5;
     }
+
     //add first
-    public void addFirst(T item){
-        if(size== items.length){
-            resize(2*size());
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(2 * size());
         }
-        items[nextFirst]=item;
-        nextFirst=(nextFirst-1+items.length)%items.length;
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
         size++;
-    };
+    }
+
+    ;
 
     //resize array
-    public void resize(int capacity) {
+    private void resize(int capacity) {
         T[] newItems = (T[]) new Object[capacity];
 
         // 按逻辑顺序从头到尾遍历现有元素，依次放入新数组的 0, 1, 2... 位置
@@ -37,104 +42,114 @@ public class ArrayDeque <T>implements Iterable<T>,Deque<T> {
         nextFirst = capacity - 1; // 新数组的头部在末尾
         nextLast = size;          // 新数组的尾部在 size 处
     }
+
     //add last
-    public void addLast(T item){
-        if(size==items.length){
-            resize(2*size());
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(2 * size());
         }
-        items[nextLast]=item;
-        nextLast=(nextLast+1)%items.length;
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) % items.length;
         size++;
     }
-    //return true when empty
-    public boolean isEmpty(){
-        if(size==0){
-            return true;
-        }else{
-            return false;
-        }
-    }
+
     //return size
-    public int size(){
+    public int size() {
         return size;
     }
+
     //print deque
-    public void printDeque(){
-        int i=nextFirst-1;
-        while(items[i]!=null){
-            System.out.print(items[i]+" ");
-            i=(i+1)%items.length;
+    public void printDeque() {
+        int i = nextFirst - 1;
+        while (items[i] != null) {
+            System.out.print(items[i] + " ");
+            i = (i + 1) % items.length;
         }
         System.out.println("\n");
     }
+
     //remove first
-    public T removeFirst(){
-        if((size<items.length/4)&&(size>=16)){
-            resize((items.length)/2);
+    public T removeFirst() {
+        if ((size < items.length / 4) && (size >= 16)) {
+            resize((items.length) / 2);
         }
-        if(size==0){
+        if (size == 0) {
             return null;
         }
-        T item=items[(nextFirst+1)%items.length];
-        nextFirst=(nextFirst+1)%items.length;
-        items[nextFirst]=null;
+        T item = items[(nextFirst + 1) % items.length];
+        nextFirst = (nextFirst + 1) % items.length;
+        items[nextFirst] = null;
         size--;
         return item;
     }
+
     //remove last
-    public T removeLast(){
-        if((size<items.length/4)&&(size>1)){
-            resize((items.length)/2);
+    public T removeLast() {
+        if ((size < items.length / 4) && (size > 1)) {
+            resize((items.length) / 2);
         }
-        if(size==0){
+        if (size == 0) {
             return null;
         }
         T item = items[(nextLast - 1 + items.length) % items.length];
         nextLast = (nextLast - 1 + items.length) % items.length;
-        items[nextLast]=null;
+        items[nextLast] = null;
         size--;
         return item;
     }
+
     //return indes th item
-    public T get(int index){
+    public T get(int index) {
         if (index < 0 || index >= size) { // 必须拦截不合法的索引
             return null;
         }
-        int i=(nextFirst+1+index+ items.length)%items.length;
+        int i = (nextFirst + 1 + index + items.length) % items.length;
         return items[i];
     }
+
     //return iterator
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new ArrayDequeIterator();
     }
-    public class ArrayDequeIterator implements Iterator<T>{
+
+    public class ArrayDequeIterator implements Iterator<T> {
         private int current;
-        public ArrayDequeIterator(){
-            this.current=(nextFirst+1)%items.length;
+
+        public ArrayDequeIterator() {
+            this.current = (nextFirst + 1) % items.length;
         }
+
         @Override
         public T next() {
-            T item=items[current];
-            current=(current+1)%items.length;
+            T item = items[current];
+            current = (current + 1) % items.length;
             return item;
         }
+
         @Override
         public boolean hasNext() {
-            if(current==nextLast){
+            if (current == nextLast) {
                 return false;
             }
             return true;
         }
     }
+
     //equals method
     @Override
     public boolean equals(Object o) {
-        if (this == o) { return true; }
-        if (o == null || !(o instanceof Deque)) { return false; } // 必须用 instanceof Deque
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !(o instanceof Deque)) {
+            return false;
+        } // 必须用 instanceof Deque
 
         Deque<T> other = (Deque<T>) o;
 
-        if (this.size() != other.size()) { return false; }
+        if (this.size() != other.size()) {
+            return false;
+        }
 
         for (int i = 0; i < this.size(); i++) {
             T item1 = this.get(i);
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,149 +2,172 @@ package deque;
 
 import java.util.Iterator;
 
-public class LinkedListDeque<T>implements Iterable<T>,Deque<T> {
+public class LinkedListDeque<T> implements Iterable<T>, Deque<T> {
     private Node sentinel;
     private int size;
+
     private class Node {
         private T item;
         private Node back;
         private Node front;
+
         public Node(T item) {
             this.item = item;
         }
-        public Node(T item, Node Back,Node Front) {
+
+        public Node(T item, Node Back, Node Front) {
             this.item = item;
-            this.back=Back;
-            this.front=Front;
+            this.back = Back;
+            this.front = Front;
         }
+
         public Node() {
             this.item = null;
         }
     }
-    public LinkedListDeque(){
+
+    public LinkedListDeque() {
         sentinel = new Node();
-        sentinel.back=sentinel;
-        sentinel.front=sentinel;
+        sentinel.back = sentinel;
+        sentinel.front = sentinel;
         size = 0;
-    };
+    }
+
+    ;
+
     //addfirst
-    public void addFirst(T item){
-        Node newnode =new Node(item,sentinel.back,sentinel);
-        sentinel.back.front=newnode;
-        sentinel.back=newnode;
+    public void addFirst(T item) {
+        Node newnode = new Node(item, sentinel.back, sentinel);
+        sentinel.back.front = newnode;
+        sentinel.back = newnode;
         size++;
     }
+
     //addlast
-    public void addLast(T item){
-        Node newnode=new Node(item,sentinel,sentinel.front);
-        sentinel.front.back=newnode;
-        sentinel.front=newnode;
+    public void addLast(T item) {
+        Node newnode = new Node(item, sentinel, sentinel.front);
+        sentinel.front.back = newnode;
+        sentinel.front = newnode;
         size++;
     }
-    //return true if is empty
-    public boolean isEmpty(){
-        if(sentinel.back==sentinel){
-            return true;
-        }else {
-            return false;
-        }
-    }
+
     //return size
-    public int size(){
+    public int size() {
         return size;
     }
+
     //print the deque
-    public void printDeque(){
-        Node temp=sentinel.back;
-        while(temp!=sentinel){
-            System.out.print(temp.item+" ");
-            temp=temp.back;
+    public void printDeque() {
+        Node temp = sentinel.back;
+        while (temp != sentinel) {
+            System.out.print(temp.item + " ");
+            temp = temp.back;
         }
         System.out.println("\n");
     }
+
     //remove and return first
-    public T removeFirst(){
-        if (sentinel.front==sentinel){
+    public T removeFirst() {
+        if (sentinel.front == sentinel) {
             return null;
         }
-        Node temp=sentinel.back;
-        sentinel.back.back.front=sentinel;
-        sentinel.back.front=null;
-        sentinel.back=sentinel.back.back;
+        Node temp = sentinel.back;
+        sentinel.back.back.front = sentinel;
+        sentinel.back.front = null;
+        sentinel.back = sentinel.back.back;
         size--;
         return temp.item;
     }
+
     //remove and return last
-    public T removeLast(){
-        if (sentinel.front==sentinel){
+    public T removeLast() {
+        if (sentinel.front == sentinel) {
             return null;
         }
-        Node temp=sentinel.front;
-        temp.front.back=sentinel;
-        sentinel.front=temp.front;
+        Node temp = sentinel.front;
+        temp.front.back = sentinel;
+        sentinel.front = temp.front;
         size--;
         return temp.item;
     }
+
     //return the index th item in the aray
-    public T get(int index){
-        if(index<0||index>=size){
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
-        Node temp=sentinel.back;
-        for(int i=0;i<index;i++){
-            temp=temp.back;
+        Node temp = sentinel.back;
+        for (int i = 0; i < index; i++) {
+            temp = temp.back;
         }
         return temp.item;
     }
+
     //recursivly return index th item
-    public T getrecursive(int index){
-        if(index<0||index>=size){
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
-        return getrecursivehelper(sentinel.back,index);
+        return getRecursivehelper(sentinel.back, index);
     }
+
     //helper function
-    private T getrecursivehelper(Node current,int n){
-        if(n==0){
+    private T getRecursivehelper(Node current, int n) {
+        if (n == 0) {
             return current.item;
         }
-        return getrecursivehelper(current.back,n-1);
+        return getRecursivehelper(current.back, n - 1);
     }
+
     //return iterator
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new LinkedListDequeIterator();
     }
+
     //implete iterator
     private class LinkedListDequeIterator implements Iterator<T> {
-        private Node current=sentinel.back;
-        public LinkedListDequeIterator(){
-            current=sentinel.back;
+        private Node current = sentinel.back;
+
+        public LinkedListDequeIterator() {
+            current = sentinel.back;
         }
+
         @Override
-        public boolean hasNext(){
-            if(current!=sentinel){
+        public boolean hasNext() {
+            if (current != sentinel) {
                 return true;
             }
             return false;
         }
+
         @Override
-        public T next(){
-            T item=current.item;
-            current=current.back;
+        public T next() {
+            T item = current.item;
+            current = current.back;
             return item;
         }
     }
+
     //equal methods
     @Override
-    public boolean equals(Object o){
-        if(this==o){return true;}
-        if(o==null||!(o instanceof Deque)){return false;}
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !(o instanceof Deque)) {
+            return false;
+        }
         Deque<T> other = (Deque<T>) o;
+        if (this.size() != other.size()) {
+            return false;
+        }
         //compare
         for (int i = 0; i < this.size(); i++) {
             T item1 = this.get(i);
             T item2 = other.get(i);
-            if (!item1.equals(item2)) { return false;}// 必须用 .equals()
+            if (!item1.equals(item2)) {
+                return false;
+            }// 必须用 .equals()
         }
         return true;
     }
